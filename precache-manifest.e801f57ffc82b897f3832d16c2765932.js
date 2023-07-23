b8c268ce1762b5a57998ce8b251904f72817cfe8
xBlood37 <123d.123b@mail.ru>
2023-07-23 09:00:38 +0300

    Updates

---
+++ precache-manifest.e801f57ffc82b897f3832d16c2765932.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57018f1be8c4d265a2a23a8700abe2b4",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "e50e82807e2701a24339",
+    "url": "/aviasales/static/css/main.009bb126.chunk.css"
+  },
+  {
+    "revision": "1162d9b898f3c456920f",
+    "url": "/aviasales/static/js/2.11cacc0e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/aviasales/static/js/2.11cacc0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50e82807e2701a24339",
+    "url": "/aviasales/static/js/main.8f5c9098.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/aviasales/static/js/main.8f5c9098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975db520e7028dd097d7",
+    "url": "/aviasales/static/js/runtime-main.e424b770.js"
+  },
+  {
+    "revision": "2599fe82fdbc9842fbe7e5473c6aceed",
+    "url": "/aviasales/static/media/uwu.2599fe82.gif"
+  }
+]);
